277a75747635d6c6e443a1690892ba6ff6b7a081
Shaz <apple@apples-MacBook-Pro.local>
2022-02-14 12:03:31 +0530

    notes changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const notes = require("../data/notes");
+//const notes = require("../data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
